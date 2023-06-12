e39c867911c4c34f946f715e425eba240fa67516
Shafiu Ibn Yushawu <yishafiu@gmail.com>
2023-06-12 16:43:15 +0000

    Implemented controllers for users and post with views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: "users#index"
+  get "users/:id", to: "users#show", as: "user"
+  get "users/:author_id/:posts", to: "posts#index", as: "user_posts"
 end
