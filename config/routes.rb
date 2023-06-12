580a9bcecdf493bb20d06e495fdb68631d2048b9
Shafiu Ibn Yushawu <yishafiu@gmail.com>
2023-06-12 23:31:04 +0000

    Included post show view, modified routes to use resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: "users#index"
-  get "users/:id", to: "users#show", as: "user"
-  get "users/:author_id/:posts", to: "posts#index", as: "user_posts"
-  get "users/:author_id/:posts/:id", to: "posts#show", as: "user_post"
+
+  resources :users, only: [:show] do
+    resources :posts, only: [:index, :show], param: :author_id
+  end
+
 end
