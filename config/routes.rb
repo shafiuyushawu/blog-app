1ffce9a543f3b4b011f40cff756bb1fb96320473
Shafiu Ibn Yushawu <yishafiu@gmail.com>
2023-06-16 17:11:27 +0000

    Included create post in post controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "users#index"
   resources :users, only: [:show] do
-    resources :posts, only: [:index, :show], param: :author_id
+    resources :posts, only: [:index, :show, :new, :create], param: :author_id
   end
   resources :posts, only: [:index, :show], param: :post_id
 end
