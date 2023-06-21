db35096540f7d736796cd95bf03e9568a95a6f4f
Shafiu Ibn Yushawu <yishafiu@gmail.com>
2023-06-21 20:32:34 +0000

    Added styling to login  and registration page

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,25 +1,25 @@
-class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User'
-  has_many :likes
-  has_many :comments
+# class Post < ApplicationRecord
+#   belongs_to :author, class_name: 'User'
+#   has_many :likes
+#   has_many :comments
 
-  after_save :update_posts_count
+#   after_save :update_posts_count
 
-  def recent_five_comments
-    comments.order(created_at: :desc).limit(5)
-  end
+#   def recent_five_comments
+#     comments.order(created_at: :desc).limit(5)
+#   end
 
-  private
+#   private
 
-  def update_posts_count
-    author.increment!(:post_counter)
-  end
+#   def update_posts_count
+#     author.increment!(:post_counter)
+#   end
 
-  validates :title, presence: true, length: { minimum: 5, maximum: 255 }
-  validates :comment_counter,
-            numericality: { only_integer: true, greater_than_or_equal_to: 0,
-                            message: 'Must be an integer ggreater than equal to zero' }
-  validates :likes_counter,
-            numericality: { only_integer: true, greater_than_or_equal_to: 0,
-                            message: 'Must be an integer ggreater than equal to zero' }
-end
+#   validates :title, presence: true, length: { minimum: 5, maximum: 255 }
+#   validates :comment_counter,
+#             numericality: { only_integer: true, greater_than_or_equal_to: 0,
+#                             message: 'Must be an integer ggreater than equal to zero' }
+#   validates :likes_counter,
+#             numericality: { only_integer: true, greater_than_or_equal_to: 0,
+#                             message: 'Must be an integer ggreater than equal to zero' }
+# end
