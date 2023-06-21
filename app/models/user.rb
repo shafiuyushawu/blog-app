94a7e750fdecb289ce155bd89eec69e0f83c36c2
Progress <ezeamakachisom2002@gmail.com>
2023-06-21 21:43:35 +0100

    Add confirmabel to device

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,18 @@
-# class User < ApplicationRecord
-#   # Include default devise modules. Others available are:
-#   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-#   devise :database_authenticatable, :registerable,
-#          :recoverable, :rememberable, :validatables
-#   has_many :likes, foreign_key: 'author_id'
-#   has_many :comments, foreign_key: 'author_id'
-#   has_many :posts, foreign_key: 'author_id'
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatables, :confirmable
+  has_many :likes, foreign_key: 'author_id'
+  has_many :comments, foreign_key: 'author_id'
+  has_many :posts, foreign_key: 'author_id'
 
-#   def three_recent_posts
-#     posts.order(created_at: :desc).limit(3)
-#   end
+  def three_recent_posts
+    posts.order(created_at: :desc).limit(3)
+  end
 
-#   validates :name, presence: true
-#   validates :post_counter,
-#             numericality: { only_integer: true, greater_than_or_equal_to: 0,
-#                             message: 'Must be an integer greater than zero(0)' }
-# end
+  validates :name, presence: true
+  validates :post_counter,
+            numericality: { only_integer: true, greater_than_or_equal_to: 0,
+                            message: 'Must be an integer greater than zero(0)' }
+end
