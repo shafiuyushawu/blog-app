4acb92c7caf86e7bf641d8c086b9a3bd414e5dd3
Shafiu Ibn Yushawu <yishafiu@gmail.com>
2023-06-24 23:44:21 +0000

    Added destroy functionalities to posts and comments with repsective buttons

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,9 @@
 class LikesController < ApplicationController
   def create
     @user = User.find(params[:user_id])
-    # @post = Post.find(params[:post_id])
     @likes = Like.create(author_id: @user.id, post_id: params[:post_id])
-    # @like.save
     if @likes.save
       redirect_to user_post_path(author_id:current_user[:id], id: params[:post_id])
-    # else
-    #   render.new
     end
   end
 end
