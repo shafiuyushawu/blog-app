1408a56614adc148913aa52dbd913c2de06383bd
Shafiu Ibn Yushawu <yishafiu@gmail.com>
2023-06-24 23:27:31 +0000

    fixed likes bug

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,13 @@
 class LikesController < ApplicationController
   def create
     @user = User.find(params[:user_id])
-    @post = Post.find(params[:post_author_id])
-    @like = @post.likes.new(author: current_user)
-
-    if @like.save
-      redirect_to user_post_path(@user, @post)
-    else
-      render.new
+    # @post = Post.find(params[:post_id])
+    @likes = Like.create(author_id: @user.id, post_id: params[:post_id])
+    # @like.save
+    if @likes.save
+      redirect_to user_post_path(author_id:current_user[:id], id: params[:post_id])
+    # else
+    #   render.new
     end
   end
 end
